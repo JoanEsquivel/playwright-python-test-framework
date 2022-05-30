16b93dc8e90df0beee1dbf720f9944e7862d6015
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-05-30 13:54:19 -0700

    Adding autocomplete pytest

--- example_test.py
+++ example_test.py
@@ -2,7 +2,11 @@
 #pytest --headed
 #pytest --browser chromium --browser webkit
 #pytest --base-url https://www.saucedemo.com/ --browser chromium --headed
-def test_example_is_working(page):
+
+#Configure Mypy typings for auto-completion
+from playwright.sync_api import Page
+
+def test_example_is_working(page: Page):
     page.goto("/")
     assert page.inner_text('title') == 'Swag Labs'
     
